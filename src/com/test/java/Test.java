bfa025947a8e4e382276a177cfd8f39df78553e2
TestVorstel <san.uttam81@gmail.com>
2019-09-02 07:24:18 -0500

    Initial commit

---
+++ src/com/test/java/Test.java
@@ -0,0 +1,14 @@
+package com.test.java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		libCalc calc = new libCalc();
+		System.out.println(calc.add(10, 20));
+		System.out.println(calc.sub(10, 20));
+		System.out.println(calc.prod(10, 20));
+		
+	
+	}
+
+}
---
+++ src/com/test/java/libCalc.java
@@ -0,0 +1,18 @@
+package com.test.java;
+
+public class libCalc {
+	public int add(int a, int b) {
+		return (a+b);
+		
+	}
+	
+	public int sub(int a, int b) {
+		return (a - b);
+		
+	}
+	
+	public int prod (int a, int b) {
+		return (a * b);
+		
+	}
+}
